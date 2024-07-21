946b90be15b8ac072a5e44a5f331bb88bc2781c9
abmdeve <tundederome@gmail.com>
2024-07-21 16:54:52 +0200

    Update the code
    Get all employees with home screen is implemented

--- lib/screens/home/home_screen.dart
+++ lib/screens/home/home_screen.dart
@@ -1,7 +1,30 @@
+import 'package:drift_project/data/local/db/app_db.dart';
+import 'package:drift_project/screens/widget/employee_card.dart';
 import 'package:flutter/material.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  late AppDb _db;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    _db = AppDb();
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    _db.close();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -10,8 +33,35 @@ class HomeScreen extends StatelessWidget {
         title: const Text("Home"),
         centerTitle: true,
       ),
-      body: Column(
-        children: [],
+      body: FutureBuilder(
+        future: _db.getEmployees(),
+        builder: (context, snapshot) {
+          final List<EmployeeEntityData>? employees = snapshot.data;
+
+          if (snapshot.connectionState != ConnectionState.done) {
+            return const Center(
+              child: CircularProgressIndicator(),
+            );
+          }
+
+          if (snapshot.hasError) {
+            return Center(
+              child: Text(
+                snapshot.error.toString(),
+                style: const TextStyle(),
+              ),
+            );
+          }
+
+          if (employees != null) {
+            return ListView.builder(
+              itemCount: employees.length,
+              itemBuilder: (context, index) =>
+                  EmployeeCard(employee: employees[index]),
+            );
+          }
+          return const Text("No data found");
+        },
       ),
       floatingActionButton: FloatingActionButton.extended(
         onPressed: () {
---
+++ lib/screens/widget/employee_card.dart
@@ -0,0 +1,59 @@
+import 'package:drift_project/data/local/db/app_db.dart';
+import 'package:flutter/material.dart';
+
+class EmployeeCard extends StatelessWidget {
+  final EmployeeEntityData employee;
+
+  const EmployeeCard({super.key, required this.employee});
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      //color: Colors.grey.shade400,
+      shape: const RoundedRectangleBorder(
+        side: BorderSide(
+          color: Colors.green,
+          style: BorderStyle.solid,
+          width: 1.2,
+        ),
+        borderRadius: BorderRadius.only(
+          topLeft: Radius.circular(16.0),
+          bottomRight: Radius.circular(16.0),
+        ),
+      ),
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(employee.id.toString()),
+            Text(
+              employee.userName,
+              style: const TextStyle(
+                color: Colors.black,
+              ),
+            ),
+            Text(
+              employee.firstName,
+              style: const TextStyle(
+                color: Colors.black,
+              ),
+            ),
+            Text(
+              employee.lastName,
+              style: const TextStyle(
+                color: Colors.black,
+              ),
+            ),
+            Text(
+              employee.dateOfBirth.toString(),
+              style: const TextStyle(
+                color: Colors.black,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
